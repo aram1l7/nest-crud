ca260188e80c371d1a8b305c44c59970c3788a70
aram1l7 <arammartirosyann16@gmail.com>
2025-02-23 00:44:24 +0400

    Added tests

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,7 +1,9 @@
+/* eslint-disable @typescript-eslint/unbound-method */
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
 import { UsersRepository } from './users.repository';
 import { NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user-dto';
 
 describe('UsersService', () => {
   let service: UsersService;
@@ -18,6 +20,7 @@ describe('UsersService', () => {
             deleteUser: jest.fn(),
             updateUser: jest.fn(),
             getUserById: jest.fn(),
+            getUsers: jest.fn(),
           },
         },
       ],
@@ -31,22 +34,83 @@ describe('UsersService', () => {
     jest.clearAllMocks();
   });
 
+  it('should create a new user and return it', async () => {
+    const createUserDto = {
+      name: 'Aram',
+      email: 'aram18m@gmail.com',
+      password: 'Aram2003',
+    } as CreateUserDto;
+
+    const user = {
+      name: 'Aram',
+      id: Date.now(),
+      password: 'Aram2003',
+      email: 'aram18m@gmail.com',
+    };
+
+    jest.spyOn(userRepository, 'createUser').mockResolvedValue(user);
+
+    const result = await service.createUser(createUserDto);
+
+    expect(userRepository.createUser).toBeCalled();
+    expect(userRepository.createUser).toBeCalledWith(createUserDto);
+
+    expect(result).toEqual(user);
+  });
+
   it('should return error if creating a user without email or invalid email', async () => {
-    userRepository.createUser = jest
-      .fn()
+    const invalidUser = {
+      name: 'Aram',
+      email: 'aram21',
+      password: 'Aram2000',
+    };
+
+    jest
+      .spyOn(userRepository, 'createUser')
       .mockRejectedValue(new Error('Invalid email'));
 
-    await expect(
-      service.createUser({ email: '', name: 'aram', password: 'Aram2003' }),
-    ).rejects.toThrow('Invalid email');
+    await expect(service.createUser(invalidUser)).rejects.toThrow(
+      'Invalid email',
+    );
+  });
+
+  it('should get all users', async () => {
+    const users = [
+      {
+        name: 'Aram',
+        id: Date.now(),
+        password: 'Aram2003',
+        email: 'aram18m@gmail.com',
+      },
+    ];
+
+    jest.spyOn(userRepository, 'getUsers').mockResolvedValue(users);
+
+    const fetchedUsers = await service.getUsers();
+    expect(fetchedUsers).toEqual(users);
   });
 
   it('should return error if trying to delete a user that does not exist', async () => {
-    userRepository.deleteUser = jest
-      .fn()
+    jest
+      .spyOn(userRepository, 'deleteUser')
       .mockRejectedValue(new NotFoundException('User not found'));
 
     await expect(service.deleteUser(999)).rejects.toThrow(NotFoundException);
+  });
+
+  it('should delete a user when it exists', async () => {
+    const existingUser = {
+      id: 5,
+      name: 'Aram',
+      email: 'aram18m@gmail.com',
+      password: 'Aram2003',
+    };
+
+    jest.spyOn(userRepository, 'deleteUser').mockResolvedValue(existingUser);
+
+    await service.deleteUser(existingUser.id);
+
+    expect(userRepository.deleteUser).toHaveBeenCalledWith(existingUser.id);
   });
 
   it('should update the user', async () => {
